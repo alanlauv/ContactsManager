97604bfa705664ebe5a7c767921b85fee6c9602c
alau645 <alau645@aucklanduni.ac.nz>
2013-10-20 15:39:26 +1300

    Added insertContact method to CDB

--- src/se206/project/ContactsDatabase.java
+++ src/se206/project/ContactsDatabase.java
@@ -1,5 +1,6 @@
 package se206.project;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
@@ -22,6 +23,18 @@ public class ContactsDatabase {
 	}
 	
 	public void insertContact(Contact c) {
+		ContentValues values = new ContentValues();
+		values.put(ContactsDatabaseHelper.CONTACT_FIRSTNAME, c.getFirstName());
+		values.put(ContactsDatabaseHelper.CONTACT_LASTNAME, c.getLastName());
+		values.put(ContactsDatabaseHelper.CONTACT_MOBILEPH, c.getMobileph());
+		values.put(ContactsDatabaseHelper.CONTACT_HOMEPH, c.getHomeph());
+		values.put(ContactsDatabaseHelper.CONTACT_PHOTO, c.getWorkph());
+		values.put(ContactsDatabaseHelper.CONTACT_EMAIL, c.getEmail());
+		values.put(ContactsDatabaseHelper.CONTACT_HOMEADD, c.getHomeph());
+		values.put(ContactsDatabaseHelper.CONTACT_DOA, c.getDoa());
+		//values.put(ContactsDatabaseHelper.CONTACT_PHOTO, c.getPhoto());
+		values.put(ContactsDatabaseHelper.CONTACT_GROUP, c.getGroup());
 		
+		database.insert(dbHelper.TABLE_CONTACTS, null, values);
 	}
 }
