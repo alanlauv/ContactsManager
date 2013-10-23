45204c6bb96df8ea80733391b134a6a22fe4e2d8
alau645 <alau645@aucklanduni.ac.nz>
2013-10-23 22:09:34 +1300

    Added most func. for clicking on item on list in EditGroups

--- src/se206/project/EditGroupsActivity.java
+++ src/se206/project/EditGroupsActivity.java
@@ -1,14 +1,23 @@
 package se206.project;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
 /**
  * This class represents the edit groups activity, which allows users to edit
  * all the groups currently in the contacts manager application.
@@ -20,11 +29,15 @@ public class EditGroupsActivity extends Activity {
 	
 	private Button buttonNewGroup;
 	private ListView listView;
+	private GroupsDatabaseHelper database = new GroupsDatabaseHelper(EditGroupsActivity.this);
+	private List<Group> groupList = new ArrayList<Group>();
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_groups);
+		
+		setupListView();
 		
 		buttonNewGroup = (Button)findViewById(R.id.groups_button_new);
 		listView = (ListView)findViewById(R.id.groups_listview);
@@ -51,6 +64,95 @@ public class EditGroupsActivity extends Activity {
 			}
 				
 		});
+	}
+	
+	private void setupListView() {
+		ContactsDatabaseHelper contactsDB = new ContactsDatabaseHelper(EditGroupsActivity.this);
+		groupList = database.getAllGroups(contactsDB.getAllContacts());
+		List<Map<String, String>> displayList =  new ArrayList<Map<String, String>>();
+		final String TEXT1 = "text1";
+		final String TEXT2 = "text2";
+		
+		for(Group group : groupList) {
+			final Map<String, String> listItemMap = new HashMap<String, String>();
+			listItemMap.put(TEXT1, group.getName());
+			listItemMap.put(TEXT2, "" + group.getCount());
+			displayList.add(listItemMap);
+		}
+		
+		final String[] fromMapKey = new String[] {TEXT1, TEXT2};
+		int[] ids = {android.R.id.text1, android.R.id.text2};
+
+		SimpleAdapter adapter = new SimpleAdapter(EditGroupsActivity.this, displayList,
+				android.R.layout.simple_list_item_2, fromMapKey, ids);
+		listView.setAdapter(adapter);
+		
+		listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> parentView, View clickedView,
+				final int clickedViewPos, long id) {
+
+				AlertDialog.Builder builder = new AlertDialog.Builder(EditGroupsActivity.this);
+
+				final Group selectedGroup = groupList.get(clickedViewPos);
+
+				builder.setTitle(selectedGroup.getName());
+				String[] contactOptions = {"View Group", "Edit Group Name", "Delete Group"};
+				builder.setItems(contactOptions, new DialogInterface.OnClickListener() {
+
+					@Override
+					public void onClick(DialogInterface dialog, int which) {
+						if (which == 0) { // View group
+							//Intent intent = new Intent();
+							//intent.setClass(EditGroupsActivity.this, ViewGroupActivity.class);
+							//intent.putExtra("Group", selectedGroup);
+							//startActivity(intent);
+						} else if (which == 1) { // Edit group name
+							AlertDialog.Builder builder = new AlertDialog.Builder(EditGroupsActivity.this);
+
+							builder.setTitle("Enter new group name:");
+							final EditText inputName = new EditText(EditGroupsActivity.this);
+							builder.setView(inputName);
+							builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+
+								@Override
+								public void onClick(DialogInterface dialog, int which) {
+									selectedGroup.setName(inputName.getText().toString());
+									refreshListView();
+								}
+							});
+						} else if (which == 2) { // Delete group
+							AlertDialog.Builder builder = new AlertDialog.Builder(EditGroupsActivity.this);
+							builder.setTitle("Delete this group?");
+							builder.setMessage("This cannot be undone!");
+							builder.setNegativeButton("Cancel", null);
+							builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+
+								@Override
+								public void onClick(DialogInterface dialog, int which) {
+									database.deleteGroup(selectedGroup);
+									groupList.remove(clickedViewPos);
+									refreshListView();
+								}
+
+							});
+							builder.setCancelable(true);
+							builder.create().show();
+						}
+						
+					}
+					
+				});
+				builder.create().show();
+			}
+			
+		});
+	}
+	
+	private void refreshListView() {
+		((BaseAdapter) listView.getAdapter()).notifyDataSetChanged();
+		listView.setSelection(0);
 	}
 
 	@Override
