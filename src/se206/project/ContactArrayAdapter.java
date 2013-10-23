fe1be681df8af9cc6605f03ea71caa3b94f23da1
alau645 <alau645@aucklanduni.ac.nz>
2013-10-23 16:31:32 +1300

    Created custom ArrayAdapter class for custom listview item

---
+++ src/se206/project/ContactArrayAdapter.java
@@ -0,0 +1,64 @@
+package se206.project;
+
+import java.util.List;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class ContactArrayAdapter extends ArrayAdapter {
+
+	private Context context;
+	private int layoutResourceId;   
+	private List<Contact> data = null;
+
+	public ContactArrayAdapter(Context context, int layoutResourceId, List<Contact> data) {
+		super(context, layoutResourceId, data);
+		this.layoutResourceId = layoutResourceId;
+		this.context = context;
+		this.data = data;
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		View row = convertView;
+		ContactHolder holder = null;
+
+		if (row == null) {
+			LayoutInflater inflater = ((Activity)context).getLayoutInflater();
+			row = inflater.inflate(layoutResourceId, parent, false);
+
+			holder = new ContactHolder();
+			holder.imgPhoto = (ImageView)row.findViewById(R.id.main_contact_img);
+			holder.txtName = (TextView)row.findViewById(R.id.main_contact_name);
+
+			row.setTag(holder);
+		}
+		else {
+			holder = (ContactHolder)row.getTag();
+		}
+
+		
+		Contact contact = data.get(position);
+		holder.txtName.setText(contact.getFullName());
+		byte[] bytesPhoto = contact.getPhoto();
+		if (bytesPhoto != null) {
+			Bitmap bmpPhoto = BitmapFactory.decodeByteArray(bytesPhoto, 0, bytesPhoto.length);
+			holder.imgPhoto.setImageBitmap(bmpPhoto);
+		}
+
+		return row;
+	}
+
+	static class ContactHolder {
+		private ImageView imgPhoto;
+		private TextView txtName;
+	}
+}
