16bab9ba811a22f24edaeed533b29eac7e4e8469
alau645 <alau645@aucklanduni.ac.nz>
2013-10-23 22:09:05 +1300

    Made Group implement Serializable to be used in an intent

--- src/se206/project/Group.java
+++ src/se206/project/Group.java
@@ -1,5 +1,6 @@
 package se206.project;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,7 +11,7 @@ import java.util.List;
  * @author Alan Lau, alau645, 2714269
  *
  */
-public class Group {
+public class Group implements Serializable {
 
 	private int id;
 	private String name;
