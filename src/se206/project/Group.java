275af02882d4fe6069df60cb0bd4da8a76731fdc
alau645 <alau645@aucklanduni.ac.nz>
2013-10-23 21:41:02 +1300

    Created more methods in Group in conjunction to GDH

--- src/se206/project/Group.java
+++ src/se206/project/Group.java
@@ -12,18 +12,58 @@ import java.util.List;
  */
 public class Group {
 
+	private int id;
 	private String name;
+	private List<Contact> groupList = new ArrayList<Contact>();
 
 	Group(String groupName) {
 		name = groupName;
 	}
 
-	public void rename(String newName) {
-		name = newName;
+	public void add(Contact contact) {
+		groupList.add(contact);
+	}
+	
+	public void remove(Contact contact) {
+		groupList.remove(contact);
 	}
 
 	public String getName() {
 		return name;
 	}
+	
+	public int getCount() {
+		return groupList.size();
+	}
+	
+	public String getIdList() {
+		String idList = "" + groupList.get(0).getID();
+		for (int i=0; i<groupList.size(); i++) {
+			idList = idList + "," + groupList.get(i).getID();
+		}
+		return idList;
+	}
+	
+	public int getID() {
+		return id;
+	}
 
+	public void setName(String newName) {
+		name = newName;
+	}
+	
+	public void setID(int id) {
+		this.id = id;
+	}
+	
+	public void setGroupList(String idList, List<Contact> contactList) {
+		String splitList[] = idList.split(",");
+		for (int i=0; i<splitList.length; i++) {
+			for (Contact c : contactList) {
+				if (c.getID() == Integer.parseInt(splitList[i])) {
+					groupList.add(c);
+				}
+			}	
+		}
+	}
 }
