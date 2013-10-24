1ac749314fdd981d9213f7d6bb52d0e623f524ec
alau645 <alau645@aucklanduni.ac.nz>
2013-10-25 03:05:15 +1300

    Commented on Main

--- src/se206/project/MainActivity.java
+++ src/se206/project/MainActivity.java
@@ -18,26 +18,29 @@ import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.Toast;
 /**
- * This class represents the main screen of the contacts manager application
+ * This class represents the main screen of the contacts manager application. Has two
+ * different views - Group and All.
  * 
  * @author Alan Lau, alau645, 2714269
  *
  */
 public class MainActivity extends Activity {
 
+	// Values used for passing between activities through intents
 	private static final int ADD_CONTACT = 1;
 	protected static final int EDIT_CONTACT = 2;
 	private static final int EDIT_GROUPS = 3;
+
+	// Animation for button click. Makes it go opaque when clicked
 	protected static final AlphaAnimation buttonClick = new AlphaAnimation(1F, 0.5F);
+
+	// Check for whether currently in Group or All view.
 	private boolean isGroupView = false;
 
 	private ListView listView;
-	private ImageButton buttonAddContact;
-	private ImageButton buttonSort;
-	private ImageButton buttonSearch;
-	private ImageButton buttonGroup;
-	private ImageButton buttonAll;
+	private ImageButton buttonAddContact, buttonSort, buttonSearch, buttonGroup, buttonAll;
 
+	// List used to feed data into adapter
 	private List<Contact> contactList = new ArrayList<Contact>();
 	private ContactsDatabaseHelper database = new ContactsDatabaseHelper(MainActivity.this);
 
@@ -53,8 +56,6 @@ public class MainActivity extends Activity {
 		buttonGroup = (ImageButton)findViewById(R.id.main_button_group);
 		buttonAll = (ImageButton)findViewById(R.id.main_button_all);
 
-		//buttonAll.setEnabled(true);
-
 		setupListView();
 
 		// Button listener for adding a new contact. Starts the add contact activity
@@ -64,10 +65,12 @@ public class MainActivity extends Activity {
 			public void onClick(final View v) {
 				v.startAnimation(buttonClick);
 
+				// Start add activity with extra info sent to the transitioning
+				// activity to determine whether it's add or edit
 				Intent intent = new Intent();
 				intent.setClass(MainActivity.this, AddEditContactActivity.class);
-				intent.putExtra("Action", ADD_CONTACT); // extra info sent to the transitioning
-				startActivityForResult(intent, ADD_CONTACT); // activity to determine whether it's add or edit
+				intent.putExtra("Action", ADD_CONTACT);
+				startActivityForResult(intent, ADD_CONTACT);
 			}
 		});
 
@@ -78,10 +81,14 @@ public class MainActivity extends Activity {
 			public void onClick(View v) {
 				v.startAnimation(buttonClick);
 
+				// Check if currently in Group view, then start EditGroups activity
+				// instead of sorting - sorting removed from Group view
 				if (listView.getAdapter().getClass() == GroupArrayAdapter.class) {
 					Intent intent = new Intent();
 					intent.setClass(MainActivity.this, EditGroupsActivity.class);
 					startActivityForResult(intent, EDIT_GROUPS);
+
+					// Currently in All view so start sorting dialog
 				} else {
 					AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
 
@@ -101,6 +108,8 @@ public class MainActivity extends Activity {
 								} else if (which == 2) {
 									Collections.sort(contactList, Contact.Comparators.MOBILEPH);
 								}
+
+								// Set isGroupView to false - just in case - then refresh view
 								isGroupView = false;
 								refreshListView();
 							}
@@ -119,6 +128,7 @@ public class MainActivity extends Activity {
 			@Override
 			public void onClick(View v) {
 				v.startAnimation(buttonClick);
+
 				AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
 
 				builder.setTitle("Search contacts:");
@@ -129,13 +139,20 @@ public class MainActivity extends Activity {
 
 					@Override
 					public void onClick(DialogInterface dialog, int which) {
-						String value = searchInput.getText().toString(); //TODO
+						String value = searchInput.getText().toString();
+
+						// Check string is "" - invalid - then toast message user
 						if (value.isEmpty()) {
 							String displayString = "Invalid search input";
 							Toast.makeText(MainActivity.this, displayString, Toast.LENGTH_LONG).show();
+
+							// Valid input string.
 						} else {
+							// Reset and get fresh data before search
 							contactList.clear();
 							contactList.addAll(database.getAllContacts());
+							
+							// Search, and add into another contact list
 							List<Contact> searchList = new ArrayList<Contact>();
 							for (Contact c : contactList) {
 								if (c.getFirstName().compareToIgnoreCase(value) == 0
@@ -144,8 +161,12 @@ public class MainActivity extends Activity {
 									searchList.add(c);
 								}
 							}
+							
+							// Display how many results were found to user
 							String displayString = searchList.size() + " result(s) found for " + value;
 							Toast.makeText(MainActivity.this, displayString, Toast.LENGTH_LONG).show();
+							
+							// Results found, clear data list and add the results into it
 							if (!searchList.isEmpty()) {
 								contactList.clear();
 								contactList.addAll(searchList);
@@ -166,9 +187,11 @@ public class MainActivity extends Activity {
 			@Override
 			public void onClick(View v) {
 				v.startAnimation(buttonClick);
-				// TODO
+
+				// Set GroupArrayAdapter with no data - refreshListView sets the data
 				isGroupView = true;
-				listView.setAdapter(new GroupArrayAdapter(MainActivity.this, R.layout.main_group_listview_item, new ArrayList<Contact>()));
+				listView.setAdapter(new GroupArrayAdapter(MainActivity.this,
+						R.layout.main_group_listview_item, new ArrayList<Contact>()));
 				refreshListView();
 			}
 		});
@@ -180,11 +203,10 @@ public class MainActivity extends Activity {
 			public void onClick(View v) {
 				v.startAnimation(buttonClick);
 
+				// Reset and get fresh data and sort by first name
 				contactList.clear();
 				contactList.addAll(database.getAllContacts());
 				Collections.sort(contactList, Contact.Comparators.FIRSTNAME);
-				//ContactArrayAdapter adapter = new ContactArrayAdapter(MainActivity.this, R.layout.main_listview_item, contactList);
-				//listView.setAdapter(adapter);
 				isGroupView = false;
 				refreshListView();
 			}
@@ -193,16 +215,17 @@ public class MainActivity extends Activity {
 	}
 
 	/**
-	 * Sets up ListView using a custom ContactArrayAdapter
+	 * Sets up ListView using a custom ArrayAdapter, default view is All view
 	 * 
 	 */
 	private void setupListView() {
 
+		// Get data and sort by first name - default
 		contactList = database.getAllContacts();
-
 		Collections.sort(contactList, Contact.Comparators.FIRSTNAME);
 
-		ContactArrayAdapter adapter = new ContactArrayAdapter(MainActivity.this, R.layout.main_listview_item, contactList);
+		ContactArrayAdapter adapter = new ContactArrayAdapter(MainActivity.this,
+				R.layout.main_listview_item, contactList);
 		listView.setAdapter(adapter);
 
 		listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -229,6 +252,7 @@ public class MainActivity extends Activity {
 							intent.setClass(MainActivity.this, ViewContactActivity.class);
 							intent.putExtra("Contact", selectedContact);
 							startActivity(intent);
+
 							// Edit contact which will start the edit contact activity
 						} else if (which == 1) {
 							Intent intent = new Intent();
@@ -236,6 +260,7 @@ public class MainActivity extends Activity {
 							intent.putExtra("Action", EDIT_CONTACT);
 							intent.putExtra("Contact", selectedContact);
 							startActivityForResult(intent, EDIT_CONTACT);
+
 							// Delete contact which will show a dialog box asking user to confirm deletion
 							// of the selected contact
 						} else if (which == 2) {
@@ -249,7 +274,7 @@ public class MainActivity extends Activity {
 								public void onClick(DialogInterface dialog, int which) {
 									database.deleteContact(selectedContact);
 									contactList.remove(clickedViewPos);
-									refreshListView();//TODO
+									refreshListView();
 								}
 
 							});
@@ -269,10 +294,18 @@ public class MainActivity extends Activity {
 	/**
 	 * Refreshes the listview to the changed underlying data set, and returns
 	 * the view back to the top of the list.
+	 * 
+	 * In all view: sort button
+	 * In group view: sort button -> add group button
 	 */
 	private void refreshListView() {
+		// Check if currently in Group view
 		if (isGroupView && listView.getAdapter().getClass() == GroupArrayAdapter.class) {
+			// Change image of sort button to add group button image
 			buttonSort.setImageResource(R.drawable.contact_group_add);
+			
+			// Get contacts from all groups and feed into data list
+			// not all contacts will show (contacts without a group)
 			GroupsDatabaseHelper groupDB = new GroupsDatabaseHelper(MainActivity.this);
 			List<Group> groupList = groupDB.getAllGroups(contactList);
 			Collections.sort(groupList);
@@ -282,13 +315,20 @@ public class MainActivity extends Activity {
 			}
 			GroupArrayAdapter adapter = new GroupArrayAdapter(MainActivity.this, R.layout.main_group_listview_item, contactList);
 			listView.setAdapter(adapter);
+			
+			// Change view from Group to All
 		} else if (!isGroupView && listView.getAdapter().getClass() == GroupArrayAdapter.class) {
+			// Change image button back to sort
 			buttonSort.setImageResource(R.drawable.sort_icon);
 			ContactArrayAdapter adapter = new ContactArrayAdapter(MainActivity.this, R.layout.main_listview_item, contactList);
 			listView.setAdapter(adapter);
+			
+			// Normal update, no change in views
 		} else {
 			((BaseAdapter) listView.getAdapter()).notifyDataSetChanged();
 		}
+
+		// Return view to top of list
 		listView.setSelection(0);
 	}
 
@@ -296,12 +336,17 @@ public class MainActivity extends Activity {
 	public void onActivityResult(int requestCode, int resultCode, Intent intent) {
 		super.onActivityResult(requestCode, resultCode, intent);
 		switch(requestCode) {
+		// Return from Add contact activity with changes
 		case (ADD_CONTACT):
 			if (resultCode == Activity.RESULT_OK) {
+				// Get contact and its ID and add into data list
 				Contact contact = (Contact) intent.getSerializableExtra("Contact");
 				int id = database.addContact(contact);
 				contact.setID(id);
 				contactList.add(contact);
+				
+				// Check if contact assigned to a group. If not then should go to All view so
+				// set isGroupView to false so contact can be shown on All view
 				if (contact.getGroup() == null) {
 					contactList.clear();
 					contactList.addAll(database.getAllContacts());
@@ -309,44 +354,30 @@ public class MainActivity extends Activity {
 					isGroupView = false;
 				}
 				refreshListView();
-
-				//updateGroups(contact); TODO
 			}
 		break;
+		// Return from Edit contact activity with changes
 		case (EDIT_CONTACT):
 			if (resultCode == Activity.RESULT_OK) {
+				// Get contact and regular updates to database and data list
 				Contact contact = (Contact) intent.getSerializableExtra("Contact");
 				database.updateContact(contact);
-				//contactList.get(id).editContact(contact); TODO
 				contactList.clear();
 				contactList.addAll(database.getAllContacts());
 				Collections.sort(contactList, Contact.Comparators.FIRSTNAME);
 				refreshListView();
-
-				//updateGroups(contact); TODO
 			}
 		break;
+		// Return from edit groups activity with changes
 		case (EDIT_GROUPS):
 			if (resultCode == Activity.RESULT_OK) {
+				// Should be in Group view, refresh data list to see changes
 				contactList.clear();
 				contactList.addAll(database.getAllContacts());
 				refreshListView();
 			}
 		}
 	}
-
-	/*public void updateGroups(Contact contact) { //TODO
-		if (contact.getGroup() != null) {
-			GroupsDatabaseHelper groupDB = new GroupsDatabaseHelper(MainActivity.this);
-			List<Group> groupList = groupDB.getAllGroups(contactList);
-			for (Group group : groupList) {
-				if (contact.getGroup().compareTo(group.getName()) == 0) {
-					group.add(contact);
-					groupDB.updateGroup(group);
-				}
-			}
-		}
-	}*/
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
