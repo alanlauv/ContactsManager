ec7d3c15542ea84a75cb440e46d736e74f8b41c4
alau645 <alau645@aucklanduni.ac.nz>
2013-10-25 01:02:19 +1300

    Added func. to view indiv group screen

--- src/se206/project/ViewGroupActivity.java
+++ src/se206/project/ViewGroupActivity.java
@@ -1,15 +1,46 @@
 package se206.project;
 
+import java.util.Collections;
+import java.util.List;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.widget.ImageButton;
+import android.widget.ListView;
 
 public class ViewGroupActivity extends Activity {
+
+	private ListView listView;
+
+	private Group group;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_view_group);
+
+		Intent intent = getIntent();
+		group = (Group) intent.getSerializableExtra("Group");
+
+		setTitle(group.getName());
+		listView = (ListView)findViewById(R.id.view_group_listview);
+
+	}
+
+	/**
+	 * Sets up ListView using a custom ContactArrayAdapter
+	 * 
+	 */
+	private void setupListView() {
+
+		List<Contact> contactList = group.getGroupList();
+
+		Collections.sort(contactList, Contact.Comparators.FIRSTNAME);
+
+		ContactArrayAdapter adapter = new ContactArrayAdapter(ViewGroupActivity.this, R.layout.main_listview_item, contactList);
+		listView.setAdapter(adapter);
 	}
 
 	@Override
