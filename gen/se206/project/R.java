32ec366649f131d0b35491dd32a9d205fc5f4131
alau645 <alau645@aucklanduni.ac.nz>
2013-10-24 20:12:00 +1300

    Added new app icon

--- gen/se206/project/R.java
+++ gen/se206/project/R.java
@@ -21,15 +21,16 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int contact_all=0x7f020000;
-        public static final int contact_all_add=0x7f020001;
-        public static final int contact_group=0x7f020002;
-        public static final int contact_group_add=0x7f020003;
-        public static final int done_icon=0x7f020004;
-        public static final int grey_android_logo=0x7f020005;
-        public static final int ic_launcher=0x7f020006;
-        public static final int search_icon=0x7f020007;
-        public static final int sort_icon=0x7f020008;
+        public static final int cm_icon=0x7f020000;
+        public static final int contact_all=0x7f020001;
+        public static final int contact_all_add=0x7f020002;
+        public static final int contact_group=0x7f020003;
+        public static final int contact_group_add=0x7f020004;
+        public static final int done_icon=0x7f020005;
+        public static final int grey_android_logo=0x7f020006;
+        public static final int ic_launcher=0x7f020007;
+        public static final int search_icon=0x7f020008;
+        public static final int sort_icon=0x7f020009;
     }
     public static final class id {
         public static final int action_settings=0x7f08003a;
