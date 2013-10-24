3521c6ed2921bf523106674ef39092960a31fc6a
alau645 <alau645@aucklanduni.ac.nz>
2013-10-25 04:18:40 +1300

    Commented on ViewGroup

--- src/se206/project/ViewGroupActivity.java
+++ src/se206/project/ViewGroupActivity.java
@@ -14,13 +14,21 @@ import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.ImageButton;
 import android.widget.ListView;
-
+/**
+ * This class represents the list view of this group and the contacts belonging to this group.
+ * User can remove a contact from this group in this view
+ * 
+ * @author Alan Lau, alau645, 2714269
+ *
+ */
 public class ViewGroupActivity extends Activity {
 
 	private ListView listView;
 	private ImageButton buttonRemove;
 
 	private Group group;
+
+	// Position of contact in the list view
 	private int itemPosition;
 
 	@Override
@@ -28,19 +36,24 @@ public class ViewGroupActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_view_group);
 
+		// Get this group from previous activity
 		Intent intent = getIntent();
 		group = (Group) intent.getSerializableExtra("Group");
 
 		setTitle(group.getName());
 		listView = (ListView)findViewById(R.id.view_group_listview);
 		buttonRemove = (ImageButton)findViewById(R.id.view_group_button_remove);
-		
+
+		setupListView();
+
 		buttonRemove.setOnClickListener(new View.OnClickListener() {
 
+			// Clicking on this button will show a dialog confirming user's removal of this contact
+			// from the group
 			@Override
 			public void onClick(View v) {
 				v.startAnimation(MainActivity.buttonClick);
-				
+
 				AlertDialog.Builder builder = new AlertDialog.Builder(ViewGroupActivity.this);
 
 				builder.setTitle("Remove this contact from " + group.getName() + "?");
@@ -50,44 +63,47 @@ public class ViewGroupActivity extends Activity {
 
 					@Override
 					public void onClick(DialogInterface dialog, int which) {
+
+						// Assign no group to contact and update it in database
 						Contact contact = group.getGroupList().get(itemPosition);
 						contact.setGroup(null);
 						ContactsDatabaseHelper contactsDB = new ContactsDatabaseHelper(ViewGroupActivity.this);
 						contactsDB.updateContact(contact);
-						
+
+						// Remove from view and refresh
 						group.getGroupList().remove(itemPosition);
 						((BaseAdapter) listView.getAdapter()).notifyDataSetChanged();
 						listView.setSelection(0);
 
+						// Tell previous activity that this group has been modified
 						setResult(Activity.RESULT_OK, new Intent());
 					}
-					
+
 				});
 				builder.create().show();
 			}
-			
-		});
 
-		setupListView();
+		});
 	}
 
 	/**
-	 * Sets up ListView using a custom ContactArrayAdapter
+	 * Sets up ListView using a custom ContactArrayAdapter and sorts by first name.
 	 * 
 	 */
 	private void setupListView() {
 
+		// Get data to feed into adapter
 		List<Contact> contactList = group.getGroupList();
 
 		Collections.sort(contactList, Contact.Comparators.FIRSTNAME);
 
-		ContactArrayAdapter adapter = new ContactArrayAdapter(ViewGroupActivity.this, R.layout.main_listview_item, contactList);
+		ContactArrayAdapter adapter = new ContactArrayAdapter(ViewGroupActivity.this,
+				R.layout.main_listview_item, contactList);
 		listView.setAdapter(adapter);
-		
+
 		listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
-			// When user clicks on a contact on the list a dialog box will show with three
-			// options: view contact, edit contact, delete contact
+			// When user clicks on a contact on the list itemPosition will update itself
 			@Override
 			public void onItemClick(AdapterView<?> parentView, View clickedView,
 					int clickedViewPos, long id) {
