8ff17034240fedafe0b4d257871b719f2149d21a
alau645 <alau645@aucklanduni.ac.nz>
2013-10-25 04:30:41 +1300

    Commented on GAA

--- src/se206/project/GroupArrayAdapter.java
+++ src/se206/project/GroupArrayAdapter.java
@@ -13,6 +13,13 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+/**
+ * This class represents a custom ArrayAdapter for displaying contacts on a Group list view.
+ * Has a ImageView, title and subtitle TextViews, and a divider to show group name (once)
+ * 
+ * @author Alan Lau, alau645, 2714269
+ *
+ */
 public class GroupArrayAdapter extends ArrayAdapter<Contact> {
 
 	private Context context;
@@ -43,26 +50,42 @@ public class GroupArrayAdapter extends ArrayAdapter<Contact> {
 
 			row.setTag(holder);
 		} else {
+			// View is unchanged, get data from cache
 			holder = (ContactHolder)row.getTag();
 		}
 
+		// Get contact data and set it into the view
 		Contact contact = data.get(position);
+
+		// contact is not assigned to group so make this view not visible
 		if (contact.getGroup() == null) {
 			holder.divider.setVisibility(View.GONE);
+
+			// First item on the group list, so set the group name on the divider
 		} else if (position == 0) {
 			holder.divider.setText(contact.getGroup());
-		} else if (data.get(position-1).getGroup() != null && contact.getGroup().compareTo(data.get(position-1).getGroup()) != 0) {
+
+			// change in groups in the group list, so set group name on the divider
+		} else if (data.get(position-1).getGroup() != null
+				&& contact.getGroup().compareTo(data.get(position-1).getGroup()) != 0) {
 			holder.divider.setText(contact.getGroup());
+
+			// contact is in between the body of the body, so divider set to not visible
 		} else {
 			holder.divider.setVisibility(View.GONE);
 		}
+
 		holder.txtName.setText(contact.getFullName());
 		holder.txtNumber.setText(contact.getMobileph());
+
+		// Check if contact has photo and convert to bitmap and set it
 		byte[] bytesPhoto = contact.getPhoto();
 		if (bytesPhoto != null) {
 			Bitmap bmpPhoto = BitmapFactory.decodeByteArray(bytesPhoto, 0, bytesPhoto.length);
 			holder.imgPhoto.setImageBitmap(bmpPhoto);
-		} else { // no photo
+
+			// no contact photo, set default photo
+		} else {
 			holder.imgPhoto.setImageResource(R.drawable.grey_android_logo);
 		}
 
