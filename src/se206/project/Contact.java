abbdeb3c68823b45ca1f3bd90ce85689fa4d1043
alau645 <alau645@aucklanduni.ac.nz>
2013-10-25 04:54:41 +1300

    Commented on Contact

--- src/se206/project/Contact.java
+++ src/se206/project/Contact.java
@@ -23,26 +23,6 @@ public class Contact implements Comparable<Contact>, Serializable {
 	private String group = null;
 	private int id;
 
-	/*
-	Contact(String firstName,
-			String lastName,
-			String mobileph,
-			String homeph,
-			String workph,
-			String email,
-			String homeAdd,
-			String doa) {
-
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.mobileph = mobileph;
-		this.homeph = homeph;
-		this.workph = workph;
-		this.email = email;
-		this.homeAdd = homeAdd;
-		this.doa = doa;
-	}*/
-
 	Contact(String firstName,
 			String lastName,
 			String mobileph,
@@ -64,6 +44,11 @@ public class Contact implements Comparable<Contact>, Serializable {
 		this.group = group;
 	}
 
+	/**
+	 * Updates this contact's fields with the parameter contact's fields
+	 * 
+	 * @param editedContact
+	 */
 	public void editContact(Contact editedContact) {
 		this.firstName = editedContact.firstName;
 		this.lastName = editedContact.lastName;
@@ -77,71 +62,152 @@ public class Contact implements Comparable<Contact>, Serializable {
 		this.photo = editedContact.photo;
 	}
 
+	/**
+	 * Returns the full name of this contact
+	 * 
+	 * @return String full name
+	 */
 	public String getFullName() {
 		return firstName + " " + lastName;
 	}
 
+	/**
+	 * Returns the first name of this contact
+	 * 
+	 * @return String first name
+	 */
 	public String getFirstName() {
 		return firstName;
 	}
 
+	/**
+	 * Returns the last name of this contact
+	 * 
+	 * @return String last name
+	 */
 	public String getLastName() {
 		return lastName;
 	}
 
+	/**
+	 * Returns the mobile phone of this contact
+	 * 
+	 * @return String mobile phone
+	 */
 	public String getMobileph() {
 		return mobileph;
 	}
 
+	/**
+	 * Returns the home phone of this contact
+	 * 
+	 * @return String home phone
+	 */
 	public String getHomeph() {
 		return homeph;
 	}
 
+	/**
+	 * Returns the work phone of this contact
+	 * 
+	 * @return String work phone
+	 */
 	public String getWorkph() {
 		return workph;
 	}
 
+	/**
+	 * Returns the email of this contact
+	 * 
+	 * @return String email
+	 */
 	public String getEmail() {
 		return email;
 	}
 
+	/**
+	 * Returns the home address of this contact
+	 * 
+	 * @return String home address
+	 */
 	public String getHomeAdd() {
 		return homeAdd;
 	}
 
+	/**
+	 * Returns the DOB of this contact
+	 * 
+	 * @return String DOB
+	 */
 	public String getDoa() {
 		return doa;
 	}
 
+	/**
+	 * Returns the group of this contact
+	 * 
+	 * @return String group
+	 */
 	public String getGroup() {
 		return group;
 	}
 
+	/**
+	 * Returns the ID of this contact
+	 * 
+	 * @return int ID
+	 */
 	public int getID() {
 		return id;
 	}
 
+	/**
+	 * Returns the photo of this contact
+	 * 
+	 * @return byte[] photo
+	 */
+	public byte[] getPhoto(){
+		return photo;
+	}
+
+	/**
+	 * Sets the ID of this contact
+	 * 
+	 * @param id
+	 */
 	public void setID(int id) {
 		this.id = id;
 	}
-	
+
+	/**
+	 * Sets the group of this contact
+	 * 
+	 * @param groupName
+	 */
 	public void setGroup(String groupName) {
 		group = groupName;
 	}
 
+	/**
+	 * Sets the photo of this contact
+	 * 
+	 * @param imageData
+	 */
 	public void setPhoto(byte[] imageData) {
-        photo = imageData;
-    }
-
-    public byte[] getPhoto(){
-        return photo;
-    }
+		photo = imageData;
+	}
 
 	@Override
 	public int compareTo(Contact c) {
 		return Comparators.FIRSTNAME.compare(this, c);
 	}
 
+	/**
+	 * Represents the comparators used to sort contact by
+	 * 
+	 * @author Alan Lau, alau645, 2714269
+	 *
+	 */
 	public static class Comparators {
 
 		public static Comparator<Contact> FIRSTNAME = new Comparator<Contact>() {
